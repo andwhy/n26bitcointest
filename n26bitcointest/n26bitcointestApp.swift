33768f1aaf6973e681e0798ac17518458bf51d88
Andrei Rozhkov <andreylanadelrey@gmail.com>
2025-04-16 19:22:37 +0200

    Initial Commit

---
+++ n26bitcointest/n26bitcointestApp.swift
@@ -0,0 +1,17 @@
+//
+//  n26bitcointestApp.swift
+//  n26bitcointest
+//
+//  Created by Maksim Ivanov on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct n26bitcointestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
